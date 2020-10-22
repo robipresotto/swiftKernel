7cbc953e4efbd41f948bce2fa34f71b58c2bed28
Robinson Presotto <robipresotto@gmail.com>
2020-10-22 15:51:20 +0200

    Creates the script to generate the kernel image

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Install MiniConda
+bash && /
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && bash Miniconda3-latest-Linux-x86_64.sh -b -p /tmp/miniconda3 && /
+
+# Clone swift jupyter repo
+git clone https://github.com/google/swift-jupyter.git && /
+cd swift-jupyter && /
+wget https://storage.googleapis.com/swift-tensorflow-artifacts/nightlies/latest/swift-tensorflow-DEVELOPMENT-ubuntu18.04.tar.gz -c swift-tensorflow.tar.gz && mkdir swift-toolchain && /
+tar -xvz swift-tensorflow.tar.gz /swift-toolchain && /
+
+# Creates the new tensorflow env
+conda create -n swift-tensorflow python=3.6 --yes && /
+conda activate swift-tensorflow --yes && /
+conda install jupyter numpy matplotlib --yes && /
+
+# Register the kernel
+python register.py --sys-prefix \
+--swift-python-use-conda \
+--use-conda-shared-libs \
+--swift-toolchain /swift-toolchain
