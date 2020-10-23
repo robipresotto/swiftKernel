bf6c8b468dedb42cd0bea5fff6ce5c77a9fc3577
Robinson Presotto <robipresotto@gmail.com>
2020-10-23 09:09:57 +0200

    Export conda env

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 
 ### Getting started
 1. Open the SageMaker Studio: File > New Launcher
-2. Select the sagemaker image to launch the activity (TensorFlow 2+ GPU)
+2. Select the SageMaker image (TensorFlow 2+ GPU) to launch the activity
 3. Select Image Terminal
-4. Run the command bellow
-
+4. Run the command bellow to create the swift kernel env
 ```
 git clone https://github.com/robipresotto/swiftKernel.git && \
 cd swiftKernel && \
-bash -i run.sh
+bash run.sh
+```
+5. The kernel isn't persisted when the SageMaker image hosting the kernel is shutdown. To re-create the kernel for a new session run the command bellow:
+```
+conda env create --file ~/swiftKernel.yml
 ```
--- run.sh
+++ run.sh
@@ -3,26 +3,30 @@
 set -eu
 
 # Install MiniConda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && bash Miniconda3-latest-Linux-x86_64.sh -b -p /tmp/miniconda3 && \
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && sh Miniconda3-latest-Linux-x86_64.sh -b -p /tmp/miniconda3 && \
 
 # Clone swift jupyter repo
-git clone https://github.com/google/swift-jupyter.git && \
-cd swift-jupyter && \
+git clone https://github.com/google/swift-jupyter.git && cd swift-jupyter && \
 wget -O ./swift-tensorflow.tar.gz https://storage.googleapis.com/swift-tensorflow-artifacts/nightlies/latest/swift-tensorflow-DEVELOPMENT-ubuntu18.04.tar.gz && \
 mkdir swift-toolchain && \
 tar -zxvf swift-tensorflow.tar.gz -C swift-toolchain && \
 
 # Creates the new tensorflow env
+conda update -n base -c defaults conda --yes && \
 conda create -n swift-tensorflow python=3.6 --yes && \
 . /tmp/miniconda3/etc/profile.d/conda.sh && \
 conda activate swift-tensorflow && \
 conda install jupyter numpy matplotlib --yes && \
 
 # Register the kernel
-python register.py --sys-prefix \
+python register.py \
+--sys-prefix \
 --swift-python-use-conda \
 --use-conda-shared-libs \
---swift-toolchain /swift-toolchain
+--swift-toolchain ./swift-toolchain
+
+# Export env
+conda env export --name swift-toolchain --file ~/swiftKernel.yml
 
 # Cleanup /tmp
 rm -rf /tmp
